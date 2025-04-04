c5e75dba42ddd6330e074bde44308b1d76260575
Administrator <admin@example.com>
2025-04-04 15:05:28 +0800

    Initial commit

---
+++ KdeBlur.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+declare -A processed_windows  # 使用关联数组存储已处理的窗口ID
+
+# 透明度值 (0-1 之间，1为不透明，0为完全透明)
+OPACITY=0.8
+
+while true; do
+    sleep 0.5  # 减少CPU使用率
+    declare -A current_windows  # 创建一个新数组来存储当前循环的窗口ID
+    current_window_ids=$(wmctrl -l | cut -d " " -f 1)  # 获取当前所有窗口的ID
+
+    for id in $current_window_ids; do
+        current_windows[$id]=1  # 标记当前窗口为存在
+        if [ -z "${processed_windows[$id]}" ]; then  # 检查窗口ID是否未被处理
+            # 设置窗口透明度
+            xprop -id $id -f _NET_WM_WINDOW_OPACITY 32c -set _NET_WM_WINDOW_OPACITY $(printf 0x%x $((${OPACITY/./}*0xffffffff/100)))
+            
+            # 设置窗口模糊效果
+            xprop -f _KDE_NET_WM_BLUR_BEHIND_REGION 32c -set _KDE_NET_WM_BLUR_BEHIND_REGION 0 -id $id
+            
+            processed_windows[$id]=1  # 记录已处理的窗口ID
+        fi
+    done
+
+    # 清除已关闭的窗口ID
+    for id in "${!processed_windows[@]}"; do
+        if [ -z "${current_windows[$id]}" ]; then
+            unset processed_windows[$id]  # 如果窗口ID不在当前窗口列表中，从processed_windows中移除
+        fi
+    done
+done
